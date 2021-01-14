37053c8d34c2eaf012ce5474d91c2fb117dca4a3
Nick Hong <zx2581396@gmail.com>
2021-01-14 15:43:29 +0800

    Motify function

--- app/src/main/java/com/example/calculator/CalculatorHelper.kt
+++ app/src/main/java/com/example/calculator/CalculatorHelper.kt
@@ -2,20 +2,12 @@ package com.example.calculator
 
 class CalculatorHelper {
     companion object {
-        fun add(left_number: Double, right_number: Double): Double {
-            return left_number + right_number
-        }
+        fun add(left_number: Double, right_number: Double) = left_number + right_number
 
-        fun subtract(left_number: Double, right_number: Double): Double {
-            return left_number - right_number
-        }
+        fun subtract(left_number: Double, right_number: Double) = left_number - right_number
 
-        fun divide(left_number: Double, right_number: Double): Double {
-            return left_number / right_number
-        }
+        fun divide(left_number: Double, right_number: Double) = left_number / right_number
 
-        fun multiply(left_number: Double, right_number: Double): Double {
-            return left_number * right_number
-        }
+        fun multiply(left_number: Double, right_number: Double) = left_number * right_number
     }
 }
